d97cfde84c908f11f13f385738feae332ec8a0a4
ttram22 <3124560092@sv.sgu.edu.vn>
2025-09-21 10:35:51 +0700

    Create ConNguoi.java

---
+++ ConNguoi.java
@@ -0,0 +1,11 @@
+package QuanLyTour;
+
+public class ConNguoi {
+    private String hoten;
+    private int tuoi;
+    private String gioitinh;
+    private String diachi;
+    private int sodienthoai;
+    
+    
+}
