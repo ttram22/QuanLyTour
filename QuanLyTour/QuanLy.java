a18b8ae79a74ac4100d1cf51176c41f9546f75e1
ttram22 <3124560092@sv.sgu.edu.vn>
2025-10-08 08:05:17 +0700

    Update QuanLy.java

--- QuanLyTour/QuanLy.java
+++ QuanLyTour/QuanLy.java
@@ -1,9 +1,11 @@
 package QuanLyTour;
 
-public interface QuanLy<T> { 
-      void them(T obj);
-      void sua(String ma); 
-      void xoa(String ma); 
-      T timKiem(String ma); 
-      void xuatDanhSach();
+public interface QuanLy<T> {
+    void them(T obj);
+    void sua(String ma);
+    void xoa(String ma);
+    T timkiem(String ma);
+    void xuatdanhsach();
+    void docfile();
+    void ghifile();
 }
