6fc842e40df7423b56feb24ff7c65b1b335fa9ff
ttram22 <3124560092@sv.sgu.edu.vn>
2025-10-14 15:17:00 +0700

    Update _AuthService.java

--- QuanLyTour/_AuthService.java
+++ QuanLyTour/_AuthService.java
@@ -5,45 +5,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _AuthService {
-      private List<Staff> StaffList = new ArrayList<>();
+      private List<_Staff> StaffList = new ArrayList<>();
 
-      // method add people is staff
       public void addPeopleCanLogin(){
             String Username;
             String Password;
             try { 
-                  BufferedReader read = new BufferedReader(new FileReader("D:\\QuanLyTour\\QuanLyTour\\DanhSachNhanVien.txt"));
+                  BufferedReader read = new BufferedReader(new FileReader("D:\\JAVA\\OOP\\QuanLyTour\\DanhSachNhanVien.txt"));
                   String line;
 
                   while ((line = read.readLine()) != null) {
                         int firstIndexOfComma = line.indexOf(",");
                         int lastIndexOfComma = line.lastIndexOf(",");
+
                         Username = line.substring(lastIndexOfComma + 1);
                         Password = line.substring(0, firstIndexOfComma);
-                        StaffList.add(new Staff(Username.trim(), Password.trim())); 
+
+                        String role;
+                        if (Password.startsWith("QL")) 
+                              role = "manager";
+                        else 
+                              role = "staff";
+
+                        StaffList.add(new _Staff(Username.trim(), Password.trim(), role.trim())); 
                   }
                   read.close();
             } 
             catch (Exception e) {
                   System.out.println("Loi khi doc file: " + e);
             }
-
       }
 
-      public _AuthService(){
-            //add staff list
+      public _AuthService() {
             addPeopleCanLogin();
       }
 
-      public boolean login(String Username, String Password){
-            for(Staff nv : StaffList){
-                  // if Username and Password equal least one in List dsNhanVien ---> return true
+      public String login(String Username, String Password) {
+            for(_Staff nv : StaffList) {
                   if(nv.getUsername().equals(Username) && nv.getPassword().equals(Password))
-                        return true;
-                  // accept login 
+                        return nv.getrole();
             }
-            // if don't has any Username and Password equal ---> return false
-            return false; 
-            // failed login 
+            return null; 
       }
 }
